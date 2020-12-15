dd405004bdbf4ee44fb50dea4cbaeed92bf62826
liubob <49805483+liubob-0@users.noreply.github.com>
2020-12-15 15:47:58 +0900

    Update install-plug.sh

--- install-plug.sh
+++ install-plug.sh
@@ -1,1 +1,2 @@
 git clone https://github.com/preservim/nerdtree.git ~/.vim/pack/vendor/start/nerdtree
+git clone --recurse-submodules https://github.com/python-mode/python-mode.git ~/.vim/pack/vendor/start/python-mode
